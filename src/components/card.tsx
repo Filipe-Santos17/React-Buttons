2c837799aa4457ee7a9abf2014e4fd47a292b8bf
Filipe <filipemard@gmail.com>
2022-10-30 14:39:54 -0300

    header and card incomplete

---
+++ src/components/card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <section>
+      <article>
+        {/*component - image para teste*/}
+      </article>
+      <article>
+        <h3>First Button center</h3>
+        <p>Descrição text left</p>
+      </article>
+    </section>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='flex items-center justify-center border-b border-black'>
+      <div className='menu-burger'></div>
+      <h1 className='title'>React Components</h1>
+      <div className='User'></div>
+    </header>
+  )
+}
